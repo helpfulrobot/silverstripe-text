67abfefb7a437c7655b7213976bdb556b246e0fc
Stephen Corwin <stephenjcorwin@gmail.com>
2015-05-18 02:29:56 -0400

    broke apart SiteConfigExtension

---
+++ code/FontSize_SiteConfigExtension.php
@@ -0,0 +1,21 @@
+<?php
+class FontSize_SiteConfigExtension extends DataExtension {
+  /**
+   * CMS FIELDS
+   */
+
+  public function updateCMSFields(FieldList $fields) {    
+    /**
+     * APPEARANCE TAB
+     */
+
+    $tab = 'Root.Appearance.Text';
+    
+    $conf=GridFieldConfig_RelationEditor::create(10);
+    $conf->removeComponentsByType('GridFieldPaginator');
+    $conf->removeComponentsByType('GridFieldPageCount');
+    $data = DataObject::get('Font');
+    $field = new GridField('Font', 'Fonts', $data, $conf);
+    $fields->addFieldToTab($tab, $field);
+  }
+}
--- code/Font_SiteConfigExtension.php
+++ code/Font_SiteConfigExtension.php
@@ -1,5 +1,5 @@
 <?php
-class Text_SiteConfigExtension extends DataExtension {
+class Font_SiteConfigExtension extends DataExtension {
   /**
    * CMS FIELDS
    */
